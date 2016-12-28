6429edc7bb9daf040f87bcf52d9f69d63876c84f
davidandrus <davidandrus.com@gmail.com>
2016-12-28 11:50:00 -0800

    feat: getting feature overlay navigation working

--- app/routes/detail/photos.js
+++ app/routes/detail/photos.js
@@ -3,9 +3,12 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     const photos = Ember.get(this.modelFor('detail'), 'photos');
+    const index = Number(params['photo_index']);
 
     return {
-      photo: photos[params['photo_index']],
+      nextIndex: index === photos.length - 1 ? 0 : index + 1,
+      prevIndex: index === 0 ? photos.length - 1 : index - 1,
+      photo: photos[index],
       thumbs: photos,
     }
   }
